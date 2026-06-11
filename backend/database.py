aaa2403b77259146275edb6ee5b16296dba9a78b
iif20 <atharazza7@gmail.com>
2026-06-11 12:01:59 +0700

    Prepare backend for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/ppdb"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:@localhost/ppdb"
+)
 
 engine = create_engine(DATABASE_URL)
 
