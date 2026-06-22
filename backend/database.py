2bfd4b4a5baaccc33d12be51055e64110f682df4
iif20 <atharazza7@gmail.com>
2026-06-22 21:19:41 +0700

    add cryptography package

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,17 @@ DATABASE_URL = os.getenv(
     "mysql+pymysql://root:@localhost/ppdb"
 )
 
-engine = create_engine(DATABASE_URL)
+connect_args = {}
+
+if "aivencloud.com" in DATABASE_URL:
+    connect_args = {
+        "ssl": {"ssl_mode": "REQUIRED"}
+    }
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
