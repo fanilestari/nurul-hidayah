027f3b4e4e1c547e6b1e85256ebf4984bec3c769
iif20 <atharazza7@gmail.com>
2026-06-22 22:48:28 +0700

    connect frontend to railway

--- frontend/src/components/PPDB.jsx
+++ frontend/src/components/PPDB.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://nurul-hidayah-production.up.railway.app";
 
 export default function PPDB() {
   const [form, setForm] = useState({
